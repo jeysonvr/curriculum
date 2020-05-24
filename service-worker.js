edb9a7bcb76774bde36f1569882e8f0f833186a0
Jeyson <jeysonvegaromero@gmail.com>
2020-05-24 14:51:57 -0500

    Juego offline - 0.1

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,18 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('cache-v1.0').then(function(cache) {
             return cache.addAll([
+                '/offline/assets/images/background.png',
+                '/offline/assets/images/booble1.png',
+                '/offline/assets/images/booble2.png',
+                '/offline/assets/images/diamonds.png',
+                '/offline/assets/images/explosion.png',
+                '/offline/assets/images/fishes.png',
+                '/offline/assets/images/horse.png',
+                '/offline/assets/images/mollusk.png',
+                '/offline/assets/images/shark.png',
+                '/offline/assets/sounds/',
+                '/offline/src/GamePlay.js',
+                '/offline/src/phaser.min.js',
                 '/offline/offline.html'
             ]);
         })
@@ -20,22 +32,33 @@ self.addEventListener('activate', event => {
     console.log('se activado');
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', e => {
 
-    const resp = fetch(event.request)
-        .then(resp => {
-            return resp;
+    // 2. Cache with network fallback
+    const respuesta = caches.match(e.request)
+        .then(res => {
+            if (res) return res;
+
+            // No existe el archivo
+
+            return fetch(e.request).then(newResp => {
+                caches.open('cache-v1.0')
+                    .then(cache => {
+                        cache.put(e.request, newResp);
+                    });
+
+                return newResp.clone();
+            });
         })
         .catch(err => {
-
-            if (event.request.headers.get('accept').includes('text/html')) {
+            if (e.request.headers.get('accept').includes('text/html')) {
                 console.log('entra');
                 return caches.match('/offline/offline.html');
             }
-
         });
 
-    event.respondWith(resp);
+
+    e.respondWith(respuesta);
 });
 
 self.addEventListener('sync', event => {
